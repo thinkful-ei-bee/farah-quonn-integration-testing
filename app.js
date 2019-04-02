aa6d789875a8bd510d46f80cf193d5a78d8415db
Farah Student <farah.gus@hotmail.com>
2019-04-02 12:21:40 -0400

    testing for array

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+app.use(morgan('common'));
+
+
+function sort(list) { 
+    for(let i = 2; i < list.length; i++){
+      let j = i;
+      while(j > 0 && list[j - 1] > list[j]){
+        let temp = list[j];
+        list[j] = list[j - 1];
+        list[j - 1] = temp;
+        j--;
+      }
+    }
+    return list;
+  }
+
+  module.exports = { sort };
---
+++ test/app.test.js
@@ -0,0 +1,14 @@
+const expect = require('chai').expect;
+const sort = require('../app'); 
+const request = require('supertest');
+const app = require('../app');
+
+describe('Sort function', () => {
+    it('should contain all numbers', () => {
+        const j = 10;
+        if(Number.isNaN(j)) {
+            return "J should be a number";
+        };
+        
+    })
+});
