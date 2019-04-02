47e2beb5193ed2cdd0220655925e5824557a0563
quonn-bernard <qber83@gmail.com>
2019-04-02 14:26:14 -0700

    added tests for sort function

--- test/app.test.js
+++ test/app.test.js
@@ -1,14 +1,36 @@
 const expect = require('chai').expect;
-const sort = require('../app'); 
+const sort = require('../app');
 const request = require('supertest');
 const app = require('../app');
 
+const result = app.sort([
+    4,
+    5,
+    7,
+    11,
+    1,
+    6
+]);
+
 describe('Sort function', () => {
     it('should contain all numbers', () => {
-        const j = 10;
-        if(Number.isNaN(j)) {
-            return "J should be a number";
-        };
-        
+        expect(result)
+            .to
+            .be
+            .an('array');
+    }),
+
+    it('should contain all numbers', () => {
+        expect(result)
+            .to
+            .deep
+            .equal([
+                1,
+                4,
+                5,
+                6,
+                7,
+                11
+            ])
     })
-});
+})
